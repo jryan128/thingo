7a33186c15729a6e0ed0d8ac684f1a2495b0a0bd
Jonathan Ryan <jonathan.ryan@oracle.com>
2014-04-23 13:16:37 -0400

    Hook up random page.

--- server.js
+++ server.js
@@ -10,13 +10,18 @@ function initializeServer(initializeFinished) {
     function makeCategory(tsvPath, finished) {
         var category = {
             name: path.basename(tsvPath, '.tsv'),
-            phrases: []
+            phrases: [],
+            freeCell: null
         };
 
         var sv = require('sv');
         var parser = new sv.Parser()
             .on('data', function (obj) {
-                category.phrases.push(obj);
+                if (!category.freeCell) {
+                    category.freeCell = obj;
+                } else {
+                    category.phrases.push(obj);
+                }
             }).
             on('finish', function () {
                 finished(null, category);
@@ -36,7 +41,7 @@ function initializeServer(initializeFinished) {
                 console.log('Shutting down server due to error...');
             } else {
                 var categories = results.reduce(function (obj, next) {
-                    obj[next.name] = next.phrases;
+                    obj[next.name] = next;
                     return obj;
                 }, {});
                 initializeFinished(categories);
@@ -54,10 +59,65 @@ function loadTemplate(categories) {
             runServer(categories, html);
         });
 }
+
+function getRandomBoardPhrases(phrases) {
+    // the board format is assumed to be flat.
+    // no hierarchy of cell placement like in the design document
+
+    // TSV format: Text \t Description
+    // - The first row is the free cell.
+    // - There are no header rows.
+
+    // return a list of randomly selected celles
+    function pickRandomCells(cells, numberOfCells) {
+        if (cells.length < numberOfCells) {
+            // TODO: handle error case
+        }
+
+        // bucket of numbers to avoid copying a bunch of objects
+        var bucket = [];
+        var cellLength = cells.length;
+        for (var i = 0; i < cellLength; i++) {
+            bucket.push(i);
+        }
+
+        // pick a random phrase from the bucket without replacement
+        function pickRandomCell() {
+            var randomIndex = Math.floor(Math.random() * bucket.length);
+            var index = bucket.splice(randomIndex, 1)[0];
+            return cells[index];
+        }
+
+        var ret = [];
+        for (var j = 0; j < numberOfCells; j++) {
+            ret.push(pickRandomCell());
+        }
+        return ret;
+    }
+
+    // cells looks like: [ [TEXT, DESCRIPTION], [TEXT, DESCRIPTION] ...]
+    // we need at least 24 random cells to fill a board, the 25th is the "Free" cell
+    var minimumCells = 24;
+    return pickRandomCells(phrases, minimumCells);
+}
+
 function runServer(categories, categoryPage) {
     app.get('/', function(req, res){
-        res.send(categoryPage);
+        res.render('index.ejs');
     });
+    app.get('/categoryPage', function(req, res){
+        res.render('categoryPage.ejs', {categories: categories});
+    });
+    app.get('/menuPage', function(req, res){
+        res.render('menuPage.ejs', {category: req.query.category});
+    });
+    app.get('/randomBoard', function(req, res){
+        var category = categories[req.query.category];
+        var phrases = getRandomBoardPhrases(category.phrases);
+        var freeCell = category.freeCell;
+        res.render('randomBoard.ejs', {phrases: phrases, freeCell: freeCell});
+    });
+
     app.use(express.static(__dirname + '/public'));
 
     console.log('Categories loaded: ' + Object.keys(categories).join(','));
