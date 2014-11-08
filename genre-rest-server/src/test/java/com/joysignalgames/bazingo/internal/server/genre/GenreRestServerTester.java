1081c50a9496bc2a3cdd3a113226e336dc5b87d1
Jonathan Ryan <jryan@curious-computing.com>
2014-11-08 17:32:54 -0500

    Add warning comment.

--- genre-rest-server/src/test/java/com/joysignalgames/bazingo/internal/server/genre/GenreRestServerTester.java
+++ genre-rest-server/src/test/java/com/joysignalgames/bazingo/internal/server/genre/GenreRestServerTester.java
@@ -4,6 +4,8 @@ import java.io.IOException;
 
 public class GenreRestServerTester {
     public static void main(String args[]) throws IOException {
+        // Set server keystore to the test one.
+        // WARNING: The test store should NEVER be used in production.
         System.getProperties().setProperty("javax.net.ssl.keyStore", "src/test/keystore/test.jks");
         System.getProperties().setProperty("javax.net.ssl.keyStorePassword", "password");
         GenreRestServer.main(args);
