339ce1558f3b6a32ecc1c191d115860722105b80
Jonathan Ryan <jryan@curious-computing.com>
2016-02-14 11:23:41 -0500

    Attempt to make BoardView more efficient

--- thingo/app/src/main/java/com/joysignal/thingo/app/board/BoardView.java
+++ thingo/app/src/main/java/com/joysignal/thingo/app/board/BoardView.java
@@ -3,10 +3,13 @@ package com.joysignal.thingo.app.board;
 import android.content.Context;
 import android.os.Handler;
 import android.view.ViewGroup;
+import com.joysignal.thingo.app.BuildConfig;
 import com.joysignal.thingo.app.R;
 
 public class BoardView extends ViewGroup {
-    private static final int NUMBER_OF_SQUARES = 25;
+    /** Must be greater than or equal to 0 */
+    private static final int NUMBER_OF_COLUMNS_AND_ROWS = 5;
+    private static final int NUMBER_OF_SQUARES = NUMBER_OF_COLUMNS_AND_ROWS ^ 2;
     public final Handler handler = new Handler();
 
     public BoardView(Context context) {
@@ -28,19 +31,18 @@ public class BoardView extends ViewGroup {
 
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-        int childCount = getChildCount(); // TODO: maybe use NUMBER_OF_SQUARES, put an assert that it's equal to getChildCount?
-        int n = getNumberOfRowsAndCols(childCount);
+        if (BuildConfig.DEBUG && getChildCount() != NUMBER_OF_SQUARES) {
+            throw new AssertionError("Expect the child count to be equal to the number of squares at all times.");
+        }
         int width = MeasureSpec.getSize(widthMeasureSpec);
         int height = MeasureSpec.getSize(heightMeasureSpec);
 
-        if (n > 0) {
-            int w = width / n;
-            int h = height / n;
-            for (int i = 0; i < childCount; ++i) {
-                // tell the child exactly what size it needs to be, screw you
-                getChildAt(i).measure(MeasureSpec.makeMeasureSpec(w, MeasureSpec.EXACTLY),
-                        MeasureSpec.makeMeasureSpec(h, MeasureSpec.EXACTLY));
-            }
+        int w = width / NUMBER_OF_COLUMNS_AND_ROWS;
+        int h = height / NUMBER_OF_COLUMNS_AND_ROWS;
+        for (int i = 0; i < NUMBER_OF_SQUARES; ++i) {
+            // tell the child exactly what size it needs to be, screw you
+            getChildAt(i).measure(MeasureSpec.makeMeasureSpec(w, MeasureSpec.EXACTLY),
+                    MeasureSpec.makeMeasureSpec(h, MeasureSpec.EXACTLY));
         }
         setMeasuredDimension(width, height);
     }
@@ -48,29 +50,24 @@ public class BoardView extends ViewGroup {
     @Override
     protected void onLayout(boolean changed, int l, int t, int r, int b) {
         // TODO: entire method could probably be more efficient
-        int n = getNumberOfRowsAndCols(getChildCount());
-        if (n > 0) {
-            int w = getMeasuredWidth() / n;
-            int h = getMeasuredHeight() / n;
-            for (int row = 0; row < n; ++row) {
-                for (int col = 0; col < n; ++col) {
-                    // TODO: should use values from onMeasure, but who cares, we know what they are
-                    // TODO: some calcs probably could be made more efficient with addition?
-                    if (row == (n - 1) && h >= w) {
-                        getChildAt((row * n) + col).layout(w * col, h * row, w * (col + 1), b);
-                    } else if (col == (n - 1) && w >= h) {
-                        getChildAt((row * n) + col).layout(w * col, h * row, r, h * (row + 1));
-                    } else {
-                        getChildAt((row * n) + col).layout(w * col, h * row, w * (col + 1), h * (row + 1));
-                    }
+        if (BuildConfig.DEBUG && getChildCount() != NUMBER_OF_SQUARES) {
+            throw new AssertionError("Expect the child count to be equal to the number of squares at all times.");
+        }
+        int n = NUMBER_OF_COLUMNS_AND_ROWS;
+        int w = getMeasuredWidth() / n;
+        int h = getMeasuredHeight() / n;
+        for (int row = 0; row < n; ++row) {
+            for (int col = 0; col < n; ++col) {
+                // TODO: should use values from onMeasure, but who cares, we know what they are
+                // TODO: some calcs probably could be made more efficient with addition?
+                if (row == (n - 1) && h >= w) {
+                    getChildAt((row * n) + col).layout(w * col, h * row, w * (col + 1), b);
+                } else if (col == (n - 1) && w >= h) {
+                    getChildAt((row * n) + col).layout(w * col, h * row, r, h * (row + 1));
+                } else {
+                    getChildAt((row * n) + col).layout(w * col, h * row, w * (col + 1), h * (row + 1));
                 }
             }
         }
-    }
-
-    private int getNumberOfRowsAndCols(int totalChildCount) {
-        // TODO: maybe use NUMBER_OF_SQUARES, put an assert that it's equal to getChildCount?
-        // TODO: sqrt?! really inefficient, should be cached with the value from NUMBER_OF_SQUARES
-        return (int) Math.floor(Math.sqrt(totalChildCount));
     }
 }
